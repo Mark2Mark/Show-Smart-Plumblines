cd6c9808f90d404f25b4edaae611b0bef51a31ff
DeutschMark <mirque84@gmail.com>
2015-11-11 14:56:40 +0100

    New Plugin

---
+++ README.md
@@ -0,0 +1,39 @@
+# Show Smart Plumblines
+
+## Plugin for Glyphsapp
+
+This is a Plugin for the [Glyphs font editor](http://glyphsapp.com/). It displays live Guidelines at the center of all paths’ (red) and components’ (grey) bounding box. If you select anything, it also displays the center of the selection (blue dashed). One major feature is that it takes the italic angle into account. This is usefull for aligning objects visually. In some cases it might match the alignment made by GlyphsApp, while in other cases you rather prefer just the visual feedback
+
+### How to use
+
+Download or clone the whole `Glyphsapp-Plugins`repo (it will contain more plugins soon) and copy the `*.glyphsReporter` into your Glyphsapp Plugins folder (eg. `/Library/Application\ Support/Glyphs/Plugins`), restart Glyphs and when ever you need it, toggle `Show Rotated` from the view menu.
+
+### Examples
+
+coming soon
+
+### Known issues
+
+- None so far
+
+### Pull Requests
+
+Feel free to comment or pull requests for any improvements.
+
+### License
+
+Copyright 2015 [Mark Frömberg](http://www.markfromberg.com/) *@DeutschMark*
+
+Made possible with the GlyphsSDK by Georg Seifert (@schriftgestalt) and Rainer Erich Scheichelbauer (@mekkablue).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ SmartPlumblines.glyphsReporter/Contents/Resources/SmartPlumblines.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# TODO:
+# 	- skew the bounds first? Currently we got bounds and skew later,
+# 		this might lead to inaccuracy? Check with anchor alignment between two points
+# 	- Badge with distance from center points if only to objects
+
+
+import objc
+# from Foundation import *
+from AppKit import *
+import sys, os, re
+import math
+
+MainBundle = NSBundle.mainBundle()
+path = MainBundle.bundlePath() + "/Contents/Scripts"
+if not path in sys.path:
+	sys.path.append( path )
+
+import GlyphsApp
+
+GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
+
+class SmartPlumblines ( NSObject, GlyphsReporterProtocol ):
+	
+	def init( self ):
+		try:
+			#Bundle = NSBundle.bundleForClass_( NSClassFromString( self.className() ));
+			return self
+		except Exception as e:
+			self.logToConsole( "init: %s" % str(e) )
+	
+	def interfaceVersion( self ):
+		try:
+			return 1
+		except Exception as e:
+			self.logToConsole( "interfaceVersion: %s" % str(e) )
+	
+	def title( self ):
+		try:
+			return "* Smart Plumblines"
+		except Exception as e:
+			self.logToConsole( "title: %s" % str(e) )
+	
+	def keyEquivalent( self ):
+		try:
+			return u"p"
+			# return None
+		except Exception as e:
+			self.logToConsole( "keyEquivalent: %s" % str(e) )
+	
+	def modifierMask( self ):
+		'''
+		return NSShiftKeyMask | NSControlKeyMask | NSCommandKeyMask | NSAlternateKeyMask
+		'''
+		try:
+			return NSCommandKeyMask | NSControlKeyMask | NSAlternateKeyMask
+			# return 0
+		except Exception as e:
+			self.logToConsole( "modifierMask: %s" % str(e) )
+	
+	def drawForegroundForLayer_( self, Layer ):
+		try:
+			pass
+		except Exception as e:
+			self.logToConsole( "drawForegroundForLayer_: %s" % str(e) )
+
+
+	def BoundsRect(self, NSRect):
+		x = NSRect[0][0]
+		y = NSRect[0][1]
+		width = NSRect[1][0]
+		height = NSRect[1][1]
+		return x, y, width, height
+
+
+	def drawLine(self, x1, y1, x2, y2):
+		strokeWidth = 1/self.getScale()
+		myPath = NSBezierPath.bezierPath()
+		myPath.moveToPoint_((x1, y1))
+		myPath.lineToPoint_((x2, y2))
+		myPath.setLineWidth_(strokeWidth)
+		if self.dashed:
+			myPath.setLineDash_count_phase_((2, 2), 2, 0.0)
+		myPath.stroke()
+
+
+	def italo(self, yPos):
+		'''
+		ITALIC OFFSET
+		'''
+		offset = math.tan(math.radians(self.angle)) * self.xHeight/2
+		shift = math.tan(math.radians(self.angle)) * yPos - offset
+		return shift
+
+
+	def italoObject(self, yPos, heightOfObject):
+		'''
+		ITALIC OFFSET
+		'''
+		### ROTATION around half object height
+		offset = math.tan(math.radians(self.angle)) * heightOfObject/2
+		shift = math.tan(math.radians(self.angle)) * yPos - offset
+		return shift
+
+
+	def DrawCross(self, x, y, width, height):
+		self.xHeight = self.layer.glyphMetrics()[4]
+		self.angle = self.layer.glyphMetrics()[5]
+
+		### BOUNDS DIMENSIONS
+		xCenter = (x + width/2)
+		xRight = x + width
+		yCenter = (y + height/2)
+		yTop = y + height
+
+		### LAYER/METRIC DIMENSIONS
+		xLayerLeft = 0
+		xLayerRight = self.layer.width
+		yAscender = self.layer.glyphMetrics()[1]
+		yDescender = self.layer.glyphMetrics()[3]
+
+		'''outside bounds'''
+		self.drawLine( xLayerLeft + self.italo(yCenter), yCenter, xLayerRight + self.italo(yCenter), yCenter)
+		### visual debugging:
+		# self.drawTextAtPoint( u"x", (xLayerLeft + self.italo(yCenter), yCenter) )
+		self.drawLine( xCenter + self.italoObject(yDescender-y, height), yDescender, xCenter + self.italoObject(yAscender-y, height), yAscender ) # without angle
+
+
+
+	def drawBackgroundForLayer_( self, Layer ):
+		try:
+			self.layer = Layer
+			pathColor = 1, 0, 0, 0.2
+			componentColor = 0, 0, 0, 0.1
+			selectionColor = 0, 0, 0.5, 0.2
+
+			'''
+			PATH
+			'''
+			self.dashed = False
+			NSColor.colorWithCalibratedRed_green_blue_alpha_( *pathColor ).set()
+			for path in Layer.paths:
+				self.DrawCross( *self.BoundsRect(path.bounds) )
+
+			'''
+			COMPONENT
+			'''
+			self.dashed = False
+			NSColor.colorWithCalibratedRed_green_blue_alpha_( *componentColor ).set()
+			for component in Layer.components:
+				self.DrawCross( *self.BoundsRect(component.bounds) )
+
+			'''
+			SELECTION
+			'''
+			if Layer.selectionBounds.origin.x < 100000: # check if Selection
+				NSColor.colorWithCalibratedRed_green_blue_alpha_( *selectionColor ).set()
+				self.dashed = True
+				self.DrawCross( *self.BoundsRect(Layer.selectionBounds) )
+
+				### DRAW BOUNDS OF SELECTION **UC**
+				# self.dashed = False
+				# sX, sY, sWidth, sHeight = self.BoundsRect(Layer.selectionBounds)
+				# sYCenter = (sY + sHeight/2)
+
+				# selectionBoundsRect = NSBezierPath.bezierPath()
+				# ### straight rect
+				# # selectionBoundsRect.moveToPoint_((sX, sY))
+				# # selectionBoundsRect.lineToPoint_((sX, sY + sHeight))
+				# # selectionBoundsRect.lineToPoint_((sX + sWidth, sY + sHeight))
+				# # selectionBoundsRect.lineToPoint_((sX + sWidth, sY))
+				# ### italic angle rect
+				# selectionBoundsRect.moveToPoint_((sX + self.italoObject(sY-sY, sHeight), sY))
+				# selectionBoundsRect.lineToPoint_((sX + self.italoObject(sY-sY + sHeight, sHeight), sY + sHeight))
+				# selectionBoundsRect.lineToPoint_((sX + sWidth + self.italoObject(sY-sY + sHeight, sHeight), sY + sHeight))
+				# selectionBoundsRect.lineToPoint_((sX + sWidth + self.italoObject(sY-sY, sHeight) , sY))
+				
+				# selectionBoundsRect.closePath()
+				# selectionBoundsRect.stroke()				
+
+
+
+
+
+		except Exception as e:
+			self.logToConsole( "drawBackgroundForLayer_: %s" % str(e) )
+	
+	def drawBackgroundForInactiveLayer_( self, Layer ):
+		try:
+			pass
+		except Exception as e:
+			self.logToConsole( "drawBackgroundForInactiveLayer_: %s" % str(e) )
+
+
+	def drawTextAtPoint( self, text, textPosition, fontSize=9.0, fontColor=NSColor.brownColor() ):
+		try:
+			glyphEditView = self.controller.graphicView()
+			currentZoom = self.getScale()
+			fontAttributes = { 
+				NSFontAttributeName: NSFont.labelFontOfSize_( fontSize/currentZoom ),
+				NSForegroundColorAttributeName: fontColor }
+			displayText = NSAttributedString.alloc().initWithString_attributes_( text, fontAttributes )
+			textAlignment = 3 # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
+			glyphEditView.drawText_atPoint_alignment_( displayText, textPosition, textAlignment )
+		except Exception as e:
+			self.logToConsole( "drawTextAtPoint: %s" % str(e) )
+	
+	def needsExtraMainOutlineDrawingForInactiveLayer_( self, Layer ):
+		return True
+	
+	def getHandleSize( self ):
+		try:
+			Selected = NSUserDefaults.standardUserDefaults().integerForKey_( "GSHandleSize" )
+			if Selected == 0:
+				return 5.0
+			elif Selected == 2:
+				return 10.0
+			else:
+				return 7.0 # Regular
+		except Exception as e:
+			self.logToConsole( "getHandleSize: HandleSize defaulting to 7.0. %s" % str(e) )
+			return 7.0
+
+	def getScale( self ):
+		try:
+			return self.controller.graphicView().scale()
+		except:
+			self.logToConsole( "Scale defaulting to 1.0" )
+			return 1.0
+	
+	def setController_( self, Controller ):
+		try:
+			self.controller = Controller
+		except Exception as e:
+			self.logToConsole( "Could not set controller" )
+	
+	def logToConsole( self, message ):
+		myLog = "Show %s plugin:\n%s" % ( self.title(), message )
+		NSLog( myLog )
